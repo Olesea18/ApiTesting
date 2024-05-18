f8c0ba37a5d32b0f4e0bb893dc30babc4829f720
Dmitri Cilcic <dmitri.cilcic@orange.com>
2024-05-18 14:49:51 +0300

    post request

--- src/test/java/tests/TestReqres.java
+++ src/test/java/tests/TestReqres.java
@@ -7,8 +7,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
-import static utils.Steps.GET;
-import static utils.Steps.isStatusCodeValid;
+import static utils.Steps.*;
 
 public class TestReqres {
 
@@ -37,4 +36,18 @@ public class TestReqres {
 
         isStatusCodeValid(response, 200);
     }
-}
+
+    @Test
+    public void testCreate(){
+        String url = "/users";
+
+        String body = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+
+        Response response = POST(body, url);
+
+        isStatusCodeValid(response, 201);
+
+    }}
--- src/test/java/utils/Steps.java
+++ src/test/java/utils/Steps.java
@@ -1,6 +1,7 @@
 package utils;
 
 import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -8,6 +9,7 @@ import static io.restassured.RestAssured.*;
 
 public class Steps {
 
+    @Step
     public static Response GET(String endpoint){
 
         Allure.addAttachment("URL:", baseURI + endpoint);
@@ -21,7 +23,24 @@ public class Steps {
         return response;
     }
 
+    @Step
     public static void isStatusCodeValid(Response response, int expectedStatusCode){
         response.then().assertThat().statusCode(expectedStatusCode);
+    }
+
+    @Step
+    public static Response POST(String body,String endpoint){
+
+        Allure.addAttachment("URL", baseURI + endpoint);
+
+        Allure.addAttachment("Request body", body);
+
+        Response response =given().body(body).post(endpoint);
+
+        Allure.addAttachment("Status Code", String.valueOf(response.statusCode()));
+
+        Allure.addAttachment("Response body", response.body().prettyPrint());
+
+        return response;
     }
 }
